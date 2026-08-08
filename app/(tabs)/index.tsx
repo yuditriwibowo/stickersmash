6d4e382a0e14bbaf6a5cfd4fd4a842e9f7fa1187
yuditriwibowo <yuditriwibowo@gmail.com>
2026-08-08 14:42:49 +0000

    ch 3: Image - CodeSpace

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,26 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import ImageViewer from "@/components/ImageViewer";
+import { StyleSheet, View } from "react-native";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>  
-      <Text style={styles.text}>Home Screen.</Text>
-      <Link href="/about">
-        <Text style={styles.button}>Go to About</Text>
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
     </View>
   );
 }
-
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
-    justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    color: "#fff",
-  },
-  button: {
-   fontSize: 20,
-   textDecorationLine: "underline",
-   color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
 });
